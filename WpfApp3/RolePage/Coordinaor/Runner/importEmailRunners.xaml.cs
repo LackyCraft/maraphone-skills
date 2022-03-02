14ec156673c9ecca050f08bf457df2708a690a39
Dany <zqxw1245@mail.ru>
2022-03-02 11:52:34 +0300

    add file importEmail

---
+++ WpfApp3/RolePage/Coordinaor/Runner/importEmailRunners.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp3.RolePage.Coordinaor.Runner
+{
+    /// <summary>
+    /// Логика взаимодействия для importEmailRunners.xaml
+    /// </summary>
+    public partial class importEmailRunners : Window
+    {
+        public importEmailRunners()
+        {
+            InitializeComponent();
+            dataGridEmail.ItemsSource = ws2016Entities5.GetContext().Participants2022.ToList();
+            InitializeComponent();
+        }
+    }
+}
