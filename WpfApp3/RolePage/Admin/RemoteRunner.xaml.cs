a8fd8cc30876add858c4a48c35dd7b3b7a21f1a4
Dany <zqxw1245@mail.ru>
2022-03-04 12:50:20 +0300

    fix remoteUser for Adminnistrator -add Serch and filter

--- WpfApp3/RolePage/Admin/RemoteRunner.xaml.cs
+++ WpfApp3/RolePage/Admin/RemoteRunner.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.SqlServer;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,15 +16,17 @@ using System.Windows.Shapes;
 
 namespace WpfApp3.RolePage.Admin
 {
-    /// <summary>
-    /// Логика взаимодействия для RemoteRunner.xaml
-    /// </summary>
     public partial class RemoteRunner : Page
     {
+
+        List<User> usersList =  ws2016Entities5.GetContext().User.ToList();
+
         public RemoteRunner()
         {
             InitializeComponent();
-            dataGridResults.ItemsSource = ws2016Entities5.GetContext().User.ToList();
+            dataGridResults.ItemsSource = usersList;
+            selectedRole.ItemsSource = ws2016Entities5.GetContext().Role.ToList(); ;
+            wayColumn.ItemsSource = new List<string>() { "Имя", "Фамилия", "Email", "Роль"};
         }
 
         private void editRunner(object sender, RoutedEventArgs e)
@@ -34,6 +37,42 @@ namespace WpfApp3.RolePage.Admin
         private void ButtonBack(object sender, RoutedEventArgs e)
         {
             this.Content = null;
+        }
+
+        private void ButtonUpdate(object sender, RoutedEventArgs e)
+        {
+            List<User> usersFilter = usersList;
+            if (!(selectedRole.SelectedValue.ToString() is null))
+            {
+                usersFilter = usersFilter.Where(i => i.Role.RoleId == selectedRole.SelectedValue.ToString()).ToList();
+            }
+             if (wayColumn.SelectedValue.ToString() == "Имя")
+             {
+                usersFilter = usersFilter.OrderBy(i => i.FirstName).ToList();
+             }
+            if (wayColumn.SelectedValue.ToString() == "Фамилия")
+            {
+                usersFilter = usersFilter.OrderBy(i => i.LastName).ToList();
+            }
+            if (wayColumn.SelectedValue.ToString() == "Email")
+            {
+                usersFilter = usersFilter.OrderBy(i => i.Email).ToList();
+            }
+            if (wayColumn.SelectedValue.ToString() == "Роль")
+            {
+                usersFilter = usersFilter.OrderBy(i => i.Role.RoleName).ToList();
+            }
+            if (!(textBoxSearch.Text.ToString() is null))
+            {
+                usersFilter = usersFilter.Where(i => (i.FirstName.Contains(textBoxSearch.Text.ToString()) || i.LastName.Contains(textBoxSearch.Text.ToString()) || i.Email.Contains(textBoxSearch.Text.ToString()) || i.Role.RoleName.Contains(textBoxSearch.Text.ToString()))).ToList();
+                //(i.FirstName == textBoxSearch.Text.ToString()) || (i.FirstName == textBoxSearch.Text.ToString()) || (i.Email == textBoxSearch.Text.ToString()) || (i.Role.RoleName == textBoxSearch.Text.ToString())
+            }
+            dataGridResults.ItemsSource = usersFilter;
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
         }
     }
 }
